6c30e43fa8cbad1b821c37299e1d19db89b80a6b
jerryzhou343 <jerry.chau@qq.com>
2020-05-31 22:46:14 +0800

    feat:增加编辑表单

--- lib/widgets/translate_task.dart
+++ lib/widgets/translate_task.dart
@@ -1,16 +1,23 @@
 import 'package:cctool/model/translate_task.dart';
 import 'package:flutter/material.dart';
-import 'package:cctool/model/choice.dart';
-import 'package:cctool/common/const.dart';
+import 'package:cctool/widgets/form_check_box.dart';
 
 Widget buildTranslateTask(BuildContext context) {
   var task = TranslateTask();
   task.from = "en";
   task.to = "zh";
   task.merge = false;
-
   var style = TextStyle(fontSize: 16, decoration: TextDecoration.none);
   final TextStyle headStyle = Theme.of(context).textTheme.headline4;
+
+  var checkBox = CheckboxFormField(
+    title: Title(color: Colors.lightBlue, child: Text("双语", style: style)),
+    context: context,
+    onSaved: (bool value) {
+      task.merge = value;
+    },
+  );
+
   //var titleStyle = TextStyle(fontSize: 20, decoration: TextDecoration.none);
   return SimpleDialog(
     title:
@@ -21,7 +28,7 @@ Widget buildTranslateTask(BuildContext context) {
         TextFormField(
           initialValue: task.from,
           decoration: InputDecoration(
-            prefixText: '源语言：',
+            prefixText: '原始语言：',
             prefixStyle: style,
           ),
           validator: (value) {
@@ -47,14 +54,49 @@ Widget buildTranslateTask(BuildContext context) {
           // 当用户确定已经完成编辑时触发
           onFieldSubmitted: (value) {},
         ),
-        CheckboxListTile(
-          title: const Text('双语'),
-          activeColor: Colors.blue,
-          value: true,
-          onChanged: (bool value) {
-            task.merge = value;
-          },
-        ),
+        checkBox,
+        Container(
+            padding: EdgeInsets.all(16),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Expanded(
+                    child: TextFormField(
+                  initialValue: task.to,
+                  decoration:
+                      InputDecoration(prefixStyle: style, prefixText: "原始文件:"),
+                  validator: (value) {
+                    return value.trim().length > 0 ? null : "不能为空";
+                  },
+                  //当 Form 表单调用保存方法 Save时回调的函数。
+                  onSaved: (value) {
+                    task.from = value;
+                  },
+                  // 当用户确定已经完成编辑时触发
+                  onFieldSubmitted: (value) {},
+                )),
+                FlatButton(onPressed: null, child: Text("浏览文件"))
+              ],
+            )),
+        Container(
+          padding: EdgeInsets.all(16),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: RaisedButton(
+                  padding: EdgeInsets.all(15),
+                  child: Text(
+                    "提交",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  textColor: Colors.white,
+                  color: Theme.of(context).primaryColor,
+                  //onPressed: login,
+                ),
+              ),
+            ],
+          ),
+        )
       ])),
     ],
   );
